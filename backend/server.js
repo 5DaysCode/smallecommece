6e3e444f89ddaa66de91819f739dc7c42820f053
codeaffection <5DaysCode@gmail.com>
2021-08-11 12:42:57 +0200

    Some minior changes

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Visma e-commerce server is runing  in  ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Small e-commerce server is runing  in  ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
