b710c57614c90c300633978faaf655f073b61af8
5DaysCode <5DaysCode@gmail.com>
2021-08-24 14:52:10 +0200

    CartUI Version onxe

--- frontend/src/UI/Cart/CartUI.js
+++ frontend/src/UI/Cart/CartUI.js
@@ -16,10 +16,93 @@ import {
 import { addToCart, removeFromCart } from "../../redux/actions/cartActions";
 
 const CartScreen = ({match, location, history}) => {
+     
+    const productId = match.params.id;
+    //getting quantity 
+    const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+    const dispatch = useDispatch();
+    
+    
+     //getting cart from the store
+     const cart = useSelector((state) => state.cart);
+     const {cartItems } = cart;
+
+    
      return(
            <>
-              Your Cart
-           </>
+  <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+     
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                    
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h5>
+                Subtotal for 
+               
+              </h5>
+    
+             0:-  SKR
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+              
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+       </>
      )
 }
 
